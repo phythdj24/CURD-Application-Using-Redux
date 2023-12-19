f87bb6c780afd6d44781a3dd2b5eb5eab4ad1b5b
ATHARV DALAL <sangamdalal24@gmail.com>
2023-12-19 15:53:55 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import MyForm from './components/Form'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Router, Route, Routes } from 'react-router-dom'
 import Read from './components/Read'
 
 function App() {
@@ -10,9 +10,10 @@ function App() {
 
   return (
     <>
-     <Navbar />
-    <BrowserRouter>
+       <Navbar />
+   
     <Routes>
+    
         
         <Route path='/' element={<MyForm />} />
         <Route path='/read' element={<Read />} />
@@ -20,7 +21,7 @@ function App() {
 
 
     </Routes>
-    </BrowserRouter>
+    
       
      
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './store/store.jsx'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <Router>
+      <App />
+      </Router>
+    
     </Provider>
     
   </React.StrictMode>,
